f42f29c402dd8fb9754904ceac77416db51267ed
yonathan_ms146 <yonathan0146@estudiantec.cr>
2025-06-17 02:03:55 -0600

    Modelo y controlador de "Branch"
    Se realizó una conexión al PostgreSQL y se realizó el controlador y el modelo de "Branch"

---
+++ GymTEC.API/Data/GymTECDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore; //el entity framework nos facilita la interacción con la base de datos relacionales mediante mapeo objeto-relacional (ORM)
+using GymTEC.API.Models;
+
+namespace GymTEC.API.Data
+{
+    // representa el contexto de la base de datos para GymTEC
+    public class GymTECDbContext : DbContext // Se agrega la herencia de DbContext
+    {
+        public GymTECDbContext(DbContextOptions<GymTECDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        //conjunto de entidades que se mapearán a la base de datos "Branches"
+        public DbSet<Branch> Branches { get; set; } = null!; // Se inicializa con null!
+    }
+}
---
+++ GymTEC.API/Migrations/20250616225120_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace GymTEC.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Branches",
+                columns: table => new
+                //creación de la tabla "Branches" con sus columnas y tipos de datos a la base PostgreSQL a traves del ORM
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Province = table.Column<string>(type: "text", nullable: false),
+                    Canton = table.Column<string>(type: "text", nullable: false),
+                    District = table.Column<string>(type: "text", nullable: false),
+                    OpeningDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    MaxCapacity = table.Column<int>(type: "integer", nullable: false),
+                    SpaEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    StoreEnabled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Branches", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Branches");
+        }
+    }
+}
